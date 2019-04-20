cd8af8d10708956fb192b9b1a98c0546b047fdc3
yaminiPonugoti <yaminiponugoti@Yaminis-MacBook-Pro.local>
2019-04-20 17:00:27 -0400

    edit app.js to show login page

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use(express.static('public'));
+
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/login.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
 
