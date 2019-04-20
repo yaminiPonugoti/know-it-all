57267e2ff6438b1157150d6f67c1de466aea866d
yaminiPonugoti <yaminiponugoti@Yaminis-MacBook-Pro.local>
2019-04-20 16:18:53 -0400

    Update

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:knowitall@know-it-all-uob80.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
