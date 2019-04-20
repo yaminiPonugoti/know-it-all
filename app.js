0f342d087589d31fa0613b266e3dbc2b38f4484c
Shameetha <jeromes@purdue.edu>
2019-04-20 16:51:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ server.listen(port, hostname, () => {
 
 
 const MongoClient = require(‘mongodb’).MongoClient;
-const uri = "mongodb+srv://admin:knowitall@know-it-all-uob80.mongodb.net/test?retryWrites=true";
+const uri = "mongodb+srv://admin1:knowitall@know-it-all-uob80.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
 const collection = client.db("test").collection("devices");
