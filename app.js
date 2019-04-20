bfbd42a6044f02c5f6da43890b4c6e123853eb9d
Dianne Santos <santos50@purdue.edu>
2019-04-20 16:31:14 -0400

    changes to app,js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*const http = require('http');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 5000;
@@ -11,32 +11,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});*/
-
-const express = require('express'),
-    cors = require('cors'),
-    app = express(),
-    port = process.env.PORT || 5000;
-    bodyParser = require('body-parser'),
-    todoRoutes = require('./routes/todo'),
-    path = require("path");
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
-app.use(express.static(path.join(__dirname, "client", "build")))
-
-app.get('/', function (req, res){
-    res.send('Root route')
-})
-
-app.use('/api/todos', todoRoutes);
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(port, function(){
-    console.log("Express server is running on port " + port)
-})
+
