66adc7ed04afa37ff05054e000aea8ecaec42ced
Shameetha <jeromes@purdue.edu>
2019-04-23 05:28:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,39 @@ var assert = require('assert');
 var url = 'mongodb:://127.0.0.1:5000/';
 
 router.get('/', function(req, res, next){
-   res.render('index');
+   res.render('login');
 });
 
 router.get('/get-data', function(req, res, next){
-
+   var resultArray = [];
+   mongo.connect(url,function(err,db){
+      assert.equal(null,err);
+      var cursor = db.collection('user-info').find();
+      cursor.forEach(function(doc,err){
+         assert(null,err);//not sure about this line assert.(null,err)
+         resultArray.push(doc);
+      }, function(){
+         db.close();
+         res.render('quiz',{items: resultArray});
+      });
+   });
 });
 
 router.get('/insert', function(req, res, next){
+   var item = {
+      username: req.body.username,
+      password: req.body.password
+   };
+
+   mongo.connect(url, function(err,db){
+      assert.equal(null,err);
+      db.collection('user-info').insertOne(item,function(err,result){
+         assert.equal(null,error);
+         console.log('User info inserted');
+         db.close();
+      });
+   });
+   res.redirect('/');
 
 });
 
