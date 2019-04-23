db24b319be505163de4ac5013f21e722de1525e4
Shameetha <jeromes@purdue.edu>
2019-04-22 21:52:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,54 @@ app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+
+//creating the schema
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
+
+
+//inserting data into MongoDb
+
+if (req.body.email &&
+  req.body.username &&
+  req.body.password &&
+  req.body.passwordConf) {
+  var userData = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  }
+  //use schema.create to insert data into the db
+  User.create(userData, function (err, user) {
+    if (err) {
+      return next(err)
+    } else {
+      return res.redirect('/profile');
+    }
+  });
+}
+
+
+/*
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://admin1:knowitall@know-it-all-uob80.mongodb.net/test?retryWrites=true";
 MongoClient.connect(url, {useNewUrlParser: true},function(err,db) {
@@ -37,7 +85,7 @@ MongoClient.connect(url, {useNewUrlParser: true},function(err,db) {
   console.log("Database created!");
   db.close();
 });
-
+*/
 
 
 
